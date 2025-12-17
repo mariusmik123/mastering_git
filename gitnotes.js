7879edeecd8377a8d09a2ecfcf4d9188d4af5580
Marius <marius.mik123@gmail.com>
2025-12-17 21:45:42 +0100

    gemmer ændringer i git notes before checkout

--- gitnotes.js
+++ gitnotes.js
@@ -1,1 +1,15 @@
+const { fileURLToPath } = require("node:url")
+
 repositery = like a folder where git keeps everything
+
+main branch = er blevet det globale konvention
+
+branches = paralellet eksisterende grene af dit projekt
+
+'git status' = tells branch, commits and untracked files
+
+'git add [filename]' = makes git track that fil
+
+'git commit -m "messeage"' = commits all changes and adds discriptive messeages
+
+'git commit '
